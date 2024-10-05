98d1447f929e1b44f50c211d55d5d06b848997e5
Jorge Navidad <jorge.espliego@outlook.com>
2024-10-05 11:52:26 +0200

    Login terminado

--- AppPatronesNombre/Presentation/Login/LoginViewController.swift
+++ AppPatronesNombre/Presentation/Login/LoginViewController.swift
@@ -3,6 +3,7 @@ import UIKit
 final class LoginViewController: UIViewController {
 
     //Estas son las conexiones de los elementos de la vista (en este caso del stackView), al arrastrar con tecla control
+    @IBOutlet private weak var sigInButton: UIButton!
     @IBOutlet private weak var errorLabel: UILabel!
     @IBOutlet private weak var spinner: UIActivityIndicatorView!
     @IBOutlet private weak var passwordField: UITextField!
@@ -27,12 +28,57 @@ final class LoginViewController: UIViewController {
         fatalError("init(coder:) has not been implemented")
     }
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        bind()
+    }
+    
     
     
     //El boton, como va a realizar una acción, en lugar de realizar una conexión Outlet se hace como acción y el compilador genera un método de acción (evento, por eso como parámetro mete un sender) al que nostros le damos el nombre de "onLoginButtonTapped" (con el boton del login pulsado)
     @IBAction func onLoginButtonTapped(_ sender: Any) {
-        viewModel.sigIn(usernameField.text, _password: passwordField.text)//LLamamos al metodo signIn del viewModel con el valor del campo de texto de las conexiones otlets que hemos establecido antes, de tal manera que cada vez que el usuario haga tap, lo mandamos al viewModel
+        viewModel.sigIn(usernameField.text, passwordField.text)//LLamamos al metodo signIn del viewModel con el valor del campo de texto de las conexiones otlets que hemos establecido antes, de tal manera que cada vez que el usuario haga tap, lo mandamos al viewModel
     }
+    
+    
+    //En esta función bind() nos suscribimos a ese binding "onStateChanged" y la llamamos arriba en el viewDidLoad
+    private func bind() {
+        viewModel.onStateChanged.bind { [weak self] state in
+        //viewModel.onStateChanged (que acabamos transformandolo en .bind tras haber programado el binding) en referencia a mi mismo (LoginViewController) vas a hacer lo siguiente con el estado de la pantalla del Login (que es lo que controla onStateChanged):
+            switch state {
+                case .success:
+                    self?.renderSuccess()
+                case .error(let reason):   //Le pasamos aquí el motivo del error, se escribe así porque le pasamos una constante como parámetro como exije el enum y le damos el mismo nombre que el que proporcionamos como parametro a la func renderError() en la siguiente linea
+                    self?.renderError(reason)
+                case .loading:
+                    self?.renderLoading()
+            }
+        }
+    }
+    
+    //podemos generar funciones para aquellos casos de los estados en la función bind() para no llenar cada caso del switch case de código. Sigue asi:
+    
+    // MARK: - State Rendering Functions
+    private func renderSuccess () {
+        sigInButton.isHidden = false
+        spinner.stopAnimating()
+        errorLabel.isHidden = true
+    }
+    
+    private func renderError(_ reason: String) { //Le pasamos aquí el motivo del error
+        sigInButton.isHidden = false
+        spinner.stopAnimating()
+        errorLabel.isHidden = false
+        errorLabel.text = reason
+    }
+    
+    private func renderLoading() {
+        sigInButton.isHidden = true
+        spinner.startAnimating()
+        errorLabel.isHidden = true
+    }
+    
+    //de esta manera en la que hacemos las funciones anteriores, se establece lo que debe hacer cada elemento según el estado de la pantalla y al switch case solo tenemos que pasarle las funciones
 }
 
 /*
--- AppPatronesNombre/Presentation/Login/LoginViewModel.swift
+++ AppPatronesNombre/Presentation/Login/LoginViewModel.swift
@@ -1,3 +1,5 @@
+import Foundation
+
 //Establecemos los estados que debe tener nuestro Login/pantalla:
 
 enum LoginState {
@@ -10,10 +12,38 @@ enum LoginState {
 
 final class LoginViewModel {
     
+    //Volvemos a utilizar el binding como en SplashViewModel
+    //Este onStateChanged lo tenemos que leer (o suscribirnos a esos cambios) en el viewDidLoad de su controller
+    let onStateChanged = Binding<LoginState>()
     //Aquí vamos a establcer las acciones que puede llevar a cabo el Login:
     
     //Acción/método de logearse a la que se les pasa como parámetros un username como String y una password como String
-    func sigIn(_ username: String?, _password: String?) {
+    func sigIn(_ username: String?, _ password: String?) {
         //Pasamos la accion/método al viewController en la acción del botón
+        //Ahora en esta función establecemos la lógica del Login:
+        // 1. Valido username valido
+        // 2. Valido password valida
+        // 3. Hago Login.
+        // Esto vamos a hacerlo en otras funciones mas abajo
+        guard let username, validateUsername(username) else { //Guard let username se encarga de asegurar que username tenga valor, es decir que no es nil, es decir, que hay algun valor en el parametro de tipo String de sigIn. después se ha de cumplir la vaidación de usuario, que es el validateUsername(username) al que se le pasa el valor del guard. Si no se cumplen esas condiciones es cuando metemos el else, que puede ser cambiar un estado o mostrar un mensaje o lo que queramos. En este caso vamos a cambiar el estado a "error" e informar el reason. Lo mismo con el password
+            return onStateChanged.update(newValue: .error(reason: "Invalid username"))
+        }
+        guard let password, validatePassword(password) else {
+            return onStateChanged.update(newValue: .error(reason: "Invalid password"))
+        }
+        
+        //Despues de que se ha validado el usuario y la contraseña se ejecuta el siguiente código:
+        onStateChanged.update(newValue: .loading) //el estado pasa a cargando y
+        DispatchQueue.global().asyncAfter(deadline: .now() + 3) { [weak self] in //tras 3 segundos desde now(),
+            self?.onStateChanged.update(newValue: .success) //el estado cambia a success
+        }
+    }
+    
+    private func validateUsername(_ username: String) -> Bool {
+        username.contains("@") && !username.isEmpty //Username tiene que contener @ (para que sea un correo) y no estar vacio
+    }
+    
+    private func validatePassword(_ password: String) -> Bool {
+        password.count >= 4  //password .count (que es lo mismo que .lenght) tiene que ser mayor o igual que 4
     }
 }
